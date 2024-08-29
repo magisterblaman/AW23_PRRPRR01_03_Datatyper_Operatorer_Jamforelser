d483bc2ff4f0866d6012234b6dc70e9e87be04fd
hannesblaman <hannes@blaman.com>
2024-08-29 12:10:35 +0200

    Type casting

--- AW23_PRRPRR01_03_Datatyper_Operatorer_Jämförelser/Program.cs
+++ AW23_PRRPRR01_03_Datatyper_Operatorer_Jämförelser/Program.cs
@@ -8,20 +8,13 @@ namespace AW23_PRRPRR01_03_Datatyper_Operatorer_Jämförelser {
 	internal class Program {
 		static void Main(string[] args) {
 
-			int a = 3;
-			int b = 3;
+			double a = 3.4;
 
-			if (a == 3 && b == 2) {
-				Console.WriteLine("If-sats 1");
-			}
+			int b = (int)a;
 
-			if (!(a == 3 || b == 2)) {
-				Console.WriteLine("If-sats 2");
-			}
+			int c = 54;
 
-			if (a == 3 && b == 2 || a + b == 6) {
-				Console.WriteLine("If-sats 3");
-			}
+			double d = c;
 		}
 	}
 }
