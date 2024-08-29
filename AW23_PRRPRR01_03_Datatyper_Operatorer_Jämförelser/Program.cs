fe05656a62635af51da3718fe21d16d1a9c0b294
hannesblaman <hannes@blaman.com>
2024-08-29 11:34:54 +0200

    Datatyper

--- AW23_PRRPRR01_03_Datatyper_Operatorer_Jämförelser/Program.cs
+++ AW23_PRRPRR01_03_Datatyper_Operatorer_Jämförelser/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace AW23_PRRPRR01_03_Datatyper_Operatorer_Jämförelser {
 	internal class Program {
 		static void Main(string[] args) {
+
+			string hejhejstring = "dsahj";
+
+			// string dethärhej = 45;
+
+			int i = 0;
+			double minVariabel = 45.3;
+
+
+			bool minBool = true;
+
+			if (minBool) { 
+
+			}
+
+			char minChar = ' ';
+
 		}
 	}
 }
